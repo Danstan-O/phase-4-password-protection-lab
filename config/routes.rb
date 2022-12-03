5b52a3221a059b510e23e9f118f459926a48d19f
danstan <danstan.oduor@student.moringaschool.com>
2022-12-03 13:11:01 +0300

    password protection and verification

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :articles, only: [:index, :show]  
+  # resources :members_only_articles, only: [:index, :show]
+  # get "/me", to: "users#show"
+  # post "/login", to: "sessions#create"
+  # delete "/logout", to: "sessions#destroy"
+  # post '/signup', to: 'users#create'
+
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
