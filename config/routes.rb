3264f4fe895735c9452c2dbb106ec566fafb816a
danstan <danstan.oduor@student.moringaschool.com>
2022-12-03 13:14:46 +0300

    password protection and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :articles, only: [:index, :show]  
-  # resources :members_only_articles, only: [:index, :show]
-  # get "/me", to: "users#show"
-  # post "/login", to: "sessions#create"
-  # delete "/logout", to: "sessions#destroy"
-  # post '/signup', to: 'users#create'
-
+Rails.application.routes.draw do  
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
